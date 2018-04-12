1177d177a54c003f09bc3d9eb07c9f45d64e3103
VENTUM\fkain <florian.kain@ventum.com>
2018-04-12 17:33:31 +0200

    tutorial 3.1

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = { 'latest_question_list': latest_question_list,}
+    return HttpResponse(render(request,"polls/index.html", context))
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're looking at the question %s." % question_id)
