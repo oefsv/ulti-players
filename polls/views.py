a6f675f9b14e7035f4f6f31ea13b1b635f504c0a
VENTUM\fkain <florian.kain@ventum.com>
2018-05-03 17:45:22 +0200

    end of tutorial 5 tests

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -12,12 +13,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
