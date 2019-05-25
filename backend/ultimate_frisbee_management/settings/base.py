8e9a6abc1e6b03f263256d64d08db50be04a9684
fkain <fk@ong.at>
2019-05-25 20:36:20 +0200

    updated setting

--- backend/ultimate_frisbee_management/settings/base.py
+++ backend/ultimate_frisbee_management/settings/base.py
@@ -34,7 +34,9 @@ ALLOWED_HOSTS = ['venv.hbqg3zr3a3.us-west-2.elasticbeanstalk.com',
 # Application definition
 
 INSTALLED_APPS = [
-    'player_management.apps.PlayerManagementConfig',
+    'viewflow',
+    'player_management',
+    #'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -83,18 +85,9 @@ WSGI_APPLICATION = 'ultimate_frisbee_management.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+  #  {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+  #  {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+  #  {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
 ]
 
 
@@ -115,22 +108,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT = 'static'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
 }
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'ultimate_frisbee_management.serializers.CustomUserDetailsSerializer',
 }
 
+ACCOUNT_LOGOUT_ON_GET = True
 FAKER_LOCALE = None     # settings.LANGUAGE_CODE is loaded
 FAKER_PROVIDERS = None  # faker.DEFAULT_PROVIDERS is loaded (all)
--- backend/ultimate_frisbee_management/settings/dev.py
+++ backend/ultimate_frisbee_management/settings/dev.py
@@ -14,18 +14,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',
-#        'USER': 'postgres',
-#        'HOST': 'localhost', # set in docker-compose.yml
-#        'PORT': 5432 # default postgres port
-#    }
-#}
 
 DATABASES = {
     'default': {
