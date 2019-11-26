bff5726b7d85dd50a739b28c840997a3f3619117
fkain <fk@ong.at>
2019-11-26 07:24:31 +0000

    update admin interface

--- backend/ultimate_frisbee_management/admin.py
+++ backend/ultimate_frisbee_management/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
-admin.site.register(models.Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('id','firstname', 'lastname', 'birthdate','sex', 'user','eligibile_u17')
+    list_display_links = ('id',)
+
+
+admin.site.register(models.Person, PersonAdmin)
 admin.site.register(models.Association)
 admin.site.register(models.Club)
 admin.site.register(models.Team)
