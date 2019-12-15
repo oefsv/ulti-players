538bdddce7ea951f9880e79a59ec99b1886381c0
fkain <fk@ong.at>
2019-12-15 09:36:58 +0100

    added jinja templating

---
+++ backend/ultimate_frisbee_management/jinja2.py
@@ -0,0 +1,16 @@
+from django.templatetags.static import static
+from django.urls import reverse
+from sesame.utils import get_query_string
+
+from jinja2 import Environment
+
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update({
+        'static': static,
+        'url': reverse,
+        'get_query_string':get_query_string,
+
+    })
+    return env
