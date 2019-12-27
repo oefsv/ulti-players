f8420eb6f524b66750bd44977a898a2be244405b
fkain <fk@ong.at>
2019-12-27 23:57:00 +0100

    pdf views

--- backend/ultimate_frisbee_management/views.py
+++ backend/ultimate_frisbee_management/views.py
@@ -206,3 +206,35 @@ class AssociationToAssociationMembershipViewSet(viewsets.ModelViewSet):
     """
     queryset = pm.AssociationToAssociationMembership.objects.all()
     serializer_class = serializers.AssociationToAssociationMembershipSerializer
+
+
+
+
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
+from .utils.pdf import render_to_pdf #created in step 4
+
+class GeneratePdf(View):
+    def get(self, request, template):
+        data = {
+             'today': datetime.date.today(), 
+             'amount': 39.99,
+            'customer_name': 'Cooper Mann',
+            'order_id': 1233434,
+        }
+        pdf = render_to_pdf(f'pdf/{template}.html', data)
+       
+        return HttpResponse(pdf, content_type='application/pdf')
+
+class dummyHtml(View):
+    def get(self, request, template):
+        data = {
+             'today': datetime.date.today(), 
+             'amount': 39.99,
+            'customer_name': 'Cooper Mann',
+            'order_id': 1233434,
+        }
+        # pdf = render_to_pdf(f'pdf/{template}.html', data)
+        pdf = get_template(f"pdf/{template}.html")
+        return HttpResponse(pdf.render(data))#, content_type='application/pdf')
