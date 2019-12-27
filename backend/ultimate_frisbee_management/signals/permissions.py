fbcf3e1b9c79a3e584b7c7b269315ca743684c67
fkain <fk@ong.at>
2019-12-27 23:59:16 +0100

    update signal recieves on Organisations

--- backend/ultimate_frisbee_management/signals/permissions.py
+++ backend/ultimate_frisbee_management/signals/permissions.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save 
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from ..models import PersonToClubMembership,Club,Person,ClubToAssociationMembership,Association,Team,PersonToTeamMembership
 from django.contrib.auth.models import User, Group
@@ -39,6 +39,13 @@ def assign_permissions_based_on_membership(instances, permission_source_obj):
                 p = f"{permission}_{obj.__class__.__name__.lower()}"
                 assign_perm(p,group,obj)
 
+def delete_permissions_for_organisation(instances, permission_source_obj):
+    permission_prefix= permission_source_obj.__class__.__name__.lower()
+
+    for role,model_permissions in permissions[permission_prefix].items():
+        group = Group.objects.get_or_create(name=f"{permission_prefix}_{role}_{permission_source_obj.name}")[0]
+        group.delete()
+
 
 @receiver(post_save,sender=PersonToClubMembership)
 def assign_permissions_based_on_club_membership(sender, instance: PersonToClubMembership, **kwargs):
@@ -51,6 +58,21 @@ def assign_permissions_based_on_club_membership(sender, instance: PersonToClubMe
     instances += PersonToClubMembership.objects.filter(club=club)
 
     assign_permissions_based_on_membership(instances,club)
+
+
+
+@receiver(post_save,sender=Team)
+@receiver(post_save,sender=Club)
+@receiver(post_save,sender=Association)
+def create_permissions_group_for_Organisation(sender, instance, **kwargs):
+    assign_permissions_based_on_membership([instance],instance)
+
+@receiver(post_delete,sender=Team)
+@receiver(post_delete,sender=Club)
+@receiver(post_delete,sender=Association)
+def delete_permissions_group_for_Organisation(sender, instance, **kwargs):
+    delete_permissions_for_organisation([instance],instance)
+
 
 @receiver(post_save,sender=PersonToTeamMembership)
 def assign_permissions_based_on_team_membership(sender, instance: PersonToTeamMembership, **kwargs):
