0be1c9eed00ec95949dd22659a4902d73df5669c
fkain <fk@ong.at>
2020-01-24 14:45:24 +0100

    collect static

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -2,5 +2,6 @@
 cd /workspace/backend
 python manage.py makemigrations --no-input      
 python manage.py migrate --no-input
+python manage.py collectstatic
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin')" | python manage.py shell
 gunicorn -c gunicorn_config.py ultimate_frisbee_management.wsgi
