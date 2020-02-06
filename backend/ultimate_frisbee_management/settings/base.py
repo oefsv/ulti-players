125607e2106da9ea3b43ac886bae7bbfaa3763ae
fkain <fk@ong.at>
2020-02-06 01:06:00 +0000

    gmail smtp

--- backend/ultimate_frisbee_management/settings/base.py
+++ backend/ultimate_frisbee_management/settings/base.py
@@ -173,7 +173,7 @@ SESAME_ONE_TIME = True
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "w0103e8b.kasserver.com"
+EMAIL_HOST = "smtp.w0103e8b.kasserver.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "admin<datenbank@frisbeeverband.at>" 
