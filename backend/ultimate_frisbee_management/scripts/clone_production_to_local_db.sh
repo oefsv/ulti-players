812d7f2983ea5c1a42bf418bd2d6a77f002e7646
fkain <fk@ong.at>
2021-02-02 00:51:17 +0000

    autmated prod to local env

---
+++ backend/ultimate_frisbee_management/scripts/clone_production_to_local_db.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# create backups
+docker -H ssh://root@db-ultimate.frisbeeverband.at exec -it ulti-players_django_1 sh -c '/usr/local/bin/python /workspace/manage.py dbbackup -o db_backup.psql && /usr/local/bin/python /workspace/manage.py mediabackup -o media_backup.tar'
+# deploy backup to local server
+docker -H ssh://root@db-ultimate.frisbeeverband.at cp ulti-players_django_1:/workspace/backups ../..
+docker -H ssh://root@db-ultimate.frisbeeverband.at exec -it ulti-players_django_1 sh -c 'rm backup/* -rf'
+docker exec -it ulti-players_django_1 sh -c '/usr/local/bin/python /workspace/manage.py dbrestore -i db_backup.psql --noinput && /usr/local/bin/python /workspace/manage.py mediarestore -i media_backup.tar --noinput'
+rm -rf ../../backups/*
